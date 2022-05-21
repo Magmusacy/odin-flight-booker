24a2ad61a7f829f2552af866ea0cc6acf8a34827
Magmusacy <xpfk.098@gmail.com>
2022-05-21 12:51:27 +0200

    Create Flight model and seed database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_090344) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_21_094444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_21_090344) do
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.bigint "departure_airport_id", null: false
+    t.bigint "arrival_airport_id", null: false
+    t.datetime "start_datetime"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,22 @@
 
 def populate_airport
   Airport.delete_all
+  ActiveRecord::Base.connection.reset_pk_sequence!('airports')
   codes = %w[SFO NYC LAX ATL DFW LAS SHA]
   codes.each { |code| Airport.create(code: code) }
 end
 
+def populate_flights
+  Flight.delete_all
+  Flight.create([
+     { departure_airport_id: 1, arrival_airport_id: 2, start_datetime: DateTime.now, duration: 69 },
+     { departure_airport_id: 2, arrival_airport_id: 3, start_datetime: DateTime.now, duration: 63 },
+     { departure_airport_id: 3, arrival_airport_id: 4, start_datetime: DateTime.now, duration: 23 },
+     { departure_airport_id: 4, arrival_airport_id: 5, start_datetime: DateTime.now, duration: 43 },
+     { departure_airport_id: 5, arrival_airport_id: 6, start_datetime: DateTime.now, duration: 32 },
+     { departure_airport_id: 6, arrival_airport_id: 7, start_datetime: DateTime.now, duration: 1029 }
+    ])
+end
+
 populate_airport
+populate_flights
