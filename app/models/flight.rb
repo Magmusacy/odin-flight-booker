606fda1c75037649272dccb60025c8ba8d3e551a
Magmusacy <xpfk.098@gmail.com>
2022-05-22 14:54:07 +0200

    Create forms for searching for a flight and booking a flight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,10 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
-  belongs_to :arrival_airport, class_name: 'Airport' #, foreign_key:
+  belongs_to :arrival_airport, class_name: 'Airport'
+
+  scope :formatted_dates, -> { order(:start_datetime).pluck(:start_datetime).map { |date| date.strftime("%d/%m/%Y") }.uniq }
+
+  def formatted_datetime
+    start_datetime.strftime("%d/%m/%Y - %H:%M")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,19 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
+def random_date
+  DateTime.now + (rand * 100).to_i
+end
+
+# This makes it possible for a flight to depart and arrive at the same airport which is pretty silly
+def random_airport_id
+  Airport.pluck(:id).sample
+end
+
+def random_duration
+  (rand * 100).to_i
+end
+
 def populate_airport
   Airport.delete_all
   ActiveRecord::Base.connection.reset_pk_sequence!('airports')
@@ -16,14 +29,8 @@ end
 
 def populate_flights
   Flight.delete_all
-  Flight.create([
-     { departure_airport_id: 1, arrival_airport_id: 2, start_datetime: DateTime.now, duration: 69 },
-     { departure_airport_id: 2, arrival_airport_id: 3, start_datetime: DateTime.now, duration: 63 },
-     { departure_airport_id: 3, arrival_airport_id: 4, start_datetime: DateTime.now, duration: 23 },
-     { departure_airport_id: 4, arrival_airport_id: 5, start_datetime: DateTime.now, duration: 43 },
-     { departure_airport_id: 5, arrival_airport_id: 6, start_datetime: DateTime.now, duration: 32 },
-     { departure_airport_id: 6, arrival_airport_id: 7, start_datetime: DateTime.now, duration: 1029 }
-    ])
+  1000.times { Flight.create({ departure_airport_id: random_airport_id, arrival_airport_id: random_airport_id, start_datetime: random_date, duration: random_duration }) }
+  Flight.create({ departure_airport_id: 4, arrival_airport_id: 4, start_datetime: DateTime.parse('21/05/2022'), duration: 0})
 end
 
 populate_airport
