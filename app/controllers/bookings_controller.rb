7709f6c3a7d16b6b5556a634181bd7a75d791f41
Magmusacy <xpfk.098@gmail.com>
2022-05-25 18:51:35 +0200

    Add Booking and improve some styling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    if query_params_present?
+      @passengers = Array.new(params[:passanger_count].to_i) { Passenger.new }
+      @booking.passengers << @passengers
+      @booking.flight_id = Flight.find(params[:flight]).id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+  end
+
+  private
+
+  def query_params_present?
+    true if [:flight, :passanger_count].all? { |el| params.has_key?(el) }
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
+# Ability to book flights that have start time before current date doesn't make much sense.
 def random_date
-  DateTime.now + (rand * 10).to_i
+  [DateTime.now + (rand * 10).to_i, DateTime.now - (rand * 10).to_i].sample
 end
 
 # This makes it possible for a flight to depart and arrive at the same airport which is pretty silly
@@ -30,7 +30,6 @@ end
 def populate_flights
   Flight.delete_all
   10000.times { Flight.create({ departure_airport_id: random_airport_id, arrival_airport_id: random_airport_id, start_datetime: random_date, duration: random_duration }) }
-  Flight.create({ departure_airport_id: 4, arrival_airport_id: 4, start_datetime: DateTime.parse('21/05/2022'), duration: 0})
 end
 
 populate_airport
