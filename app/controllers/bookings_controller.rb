4f6906978770b432ff992100c925a6abc4f8881a
Magmusacy <xpfk.098@gmail.com>
2022-05-27 11:41:33 +0200

    Complete Booking and update styling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,21 +1,41 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+
     if query_params_present?
-      @passengers = Array.new(params[:passanger_count].to_i) { Passenger.new }
-      @booking.passengers << @passengers
-      @booking.flight_id = Flight.find(params[:flight]).id
+      prepare_booking
     else
       redirect_to root_path
     end
   end
 
   def create
+    @booking = Booking.new(booking_parameters)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
   end
 
   private
 
+  def booking_parameters
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email] )
+  end
+
   def query_params_present?
     true if [:flight, :passanger_count].all? { |el| params.has_key?(el) }
+  end
+
+  def prepare_booking
+    @passengers = Array.new(params[:passanger_count].to_i) { Passenger.new }
+    @booking.passengers << @passengers
+    @booking.flight_id = Flight.find(params[:flight]).id
   end
 end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,23 +1,15 @@
 class FlightsController < ApplicationController
   def index
-    if query_params_present?
-      @available_flights = Flight.where(departure_airport_id: params[:departure_airport])
-                                 .where(arrival_airport_id:   params[:arrival_airport])
-                                 # this weird looking conversion must be present because LIKE isn't available for datetime types in sql
-                                 .where("to_char(start_datetime, 'dd/mm/yyyy') LIKE ?", params[:date])
-                                 # avoid N + 1 queries problem
-                                 .includes(:departure_airport, :arrival_airport)
-      @passanger_count = params[:passanger_count]
-    end
+    @available_flights = Flight.where(departure_airport_id: params[:departure_airport])
+                                .where(arrival_airport_id:   params[:arrival_airport])
+                                # this weird looking conversion must be present because LIKE isn't available for datetime types in sql
+                                .where("to_char(start_datetime, 'dd/mm/yyyy') LIKE ?", params[:date])
+                                # avoid N + 1 queries problem
+                                .includes(:departure_airport, :arrival_airport)
 
+    @passanger_count = params[:passanger_count]
     @departure_airports = Airport.departure_airports
     @arrival_airports = Airport.arrival_airports
     @available_dates = Flight.formatted_dates
-  end
-
-  private
-
-  def query_params_present?
-    true if [:departure_airport, :arrival_airport, :passanger_count, :date].all? { |el| params.has_key?(el) }
   end
 end
